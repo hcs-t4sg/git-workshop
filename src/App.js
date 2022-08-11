fe474f99b938a359156738f48a4210dd89c70442
Jeremy Lee <jeremylee@college.harvard.edu>
2022-08-11 13:46:07 -0400

    new description

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
                 Welcome to the T4SG Git workshop!
               </Typography>
               <Typography variant="body1" align="center" color="text.secondary" paragraph>
-                We love tech and social impact!
+                Other description
               </Typography>
               <Stack
                 sx={{ pt: 4 }}
