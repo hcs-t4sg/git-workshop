64514259cd9264a12e714cf3186e2acf2db034ca
Matthew Su <matthew.su.2019@gmail.com>
2022-08-23 21:39:58 -0500

    reset

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
                 Welcome to the T4SG Git workshop!
               </Typography>
               <Typography variant="body1" align="center" color="text.secondary" paragraph>
-                This section will be used for later.
+                Future Description.
               </Typography>
               <Stack
                 sx={{ pt: 4 }}
--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+<<<<<<< HEAD
 
-// Test message for saving commit
+=======
+>>>>>>> parent of e19eb9a... backup commit
 reportWebVitals();
