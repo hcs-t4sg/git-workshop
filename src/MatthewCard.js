572701503d9266dca826cd4560d950e941489cf3
Matthew Su <matthew.su.2019@gmail.com>
2023-01-16 16:36:10 -0600

    added my own card

---
+++ src/MatthewCard.js
@@ -0,0 +1,35 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+export default function MatthewCard() {
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card
+        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+      >
+        <CardMedia
+          component="img"
+          sx={{
+            // 16:9
+            pt: '56.25%',
+          }}
+          image="https://source.unsplash.com/random"
+          alt="random"
+        />
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {/* Name here */}
+            Matthew Su
+          </Typography>
+          <Typography>
+            {/* Harvard intro here */}
+            Junior in Cabot studying chemistry!
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  )
+}
