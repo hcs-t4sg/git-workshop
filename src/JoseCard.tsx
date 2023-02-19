827b656853f185965bdf41ea42e1c7819c8266e2
Jose Garcia <josegarcia@dhcp-10-250-198-190.harvard.edu>
2023-02-19 17:34:58 -0500

    added jose card

---
+++ src/JoseCard.tsx
@@ -0,0 +1,37 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import img from './static/t4sg_logo.jpg';
+
+export default function JoseCard() {
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card
+        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+      >
+        <CardMedia
+           component="img"
+           sx={{
+             // 16:9
+             pt: '5%',
+           }}
+           style={{ width: '80%', margin: 'auto' }}
+           src={img}
+           alt="t4sg logo"
+         />
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {/* Name here */}
+            Jose
+          </Typography>
+          <Typography>
+            {/* Harvard intro here */}
+            Intro for jose. 
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  )
+}
