17cf5b8f1982b0f7f8f4a69a1401e1155bde9de3
Matthew Su <matthew.su.2019@gmail.com>
2023-02-19 14:50:16 -0500

    changed picture on card to t4sg logo

--- src/CardTemplate.tsx
+++ src/CardTemplate.tsx
@@ -3,6 +3,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import img from './static/t4sg_logo.jpg';
 
 export default function CardTemplate() {
   return (
@@ -11,14 +12,15 @@ export default function CardTemplate() {
         sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
       >
         <CardMedia
-          component="img"
-          sx={{
-            // 16:9
-            pt: '56.25%',
-          }}
-          image="https://source.unsplash.com/random"
-          alt="random"
-        />
+           component="img"
+           sx={{
+             // 16:9
+             pt: '5%',
+           }}
+           style={{ width: '80%', margin: 'auto' }}
+           src={img}
+           alt="t4sg logo"
+         />
         <CardContent sx={{ flexGrow: 1 }}>
           <Typography gutterBottom variant="h5" component="h2">
             {/* Name here */}
